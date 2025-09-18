632fab3ad2a675914675f0124cbb3e1b93c20fc1
johnkelvinnavarro79-ra <johnkelvinnavarro79@gmail.com>
2025-09-18 12:24:12 +0800

    Add files via upload

---
+++ problem_4.py
@@ -0,0 +1,26 @@
+def isPalindrome(valStr):
+    return valStr == valStr[::-1]
+
+def toBinaryIfNumber(value):
+    if value.isdigit():
+        num = int(value)
+        return bin(num)[2:]
+    else:
+        return None
+
+user_input = input("Enter a value: ")g
+
+if isPalindrome(user_input):
+    print("Input is a palindrome: True")
+else:
+    print("Input is a palindrome: False")
+
+binary_val = toBinaryIfNumber(user_input)
+if binary_val is not None:
+    print("Binary equivalent:", binary_val)
+    if isPalindrome(binary_val):
+        print("Binary is a palindrome: True")
+    else:
+        print("Binary is a palindrome: False")
+else:
+    print("(No binary conversion since input is text)")
